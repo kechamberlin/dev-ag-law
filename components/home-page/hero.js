138e7bad1fe19808e5631d5d5379166dfa4c72e9
Kris Chamberlin <kris.elliott.chamberlin@gmail.com>
2021-06-16 09:09:38 -0700

    Created hero component and css

---
+++ components/home-page/hero.js
@@ -0,0 +1,9 @@
+import classes from './hero.module.css';
+
+export default function Hero() {
+  return (
+    <section className={classes.hero}>
+      <p>Car Accident? DUI? AG Law will help YOU get your life back</p>
+    </section>
+  );
+}
