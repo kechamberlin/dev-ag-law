0f51b30c783e40c7e1dbca8f59d8958327c1fdf0
Kris Chamberlin <kris.elliott.chamberlin@gmail.com>
2021-06-16 10:25:04 -0700

    Added disclaimer to footer

--- components/footer/footer.js
+++ components/footer/footer.js
@@ -5,6 +5,10 @@ export default function Footer() {
     <footer className={classes.footer}>
       <p></p>
       <p>&copy; Built by Kris Chamberlin | {new Date().getFullYear()}</p>
+      <p>
+        DISCLAIMER: this website is strictly for portfolio purposes. It is NOT
+        to be used for real legal consultation or contact.
+      </p>
     </footer>
   );
 }
