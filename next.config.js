a0e183b2de173667036d1f4a27cfbdaf00372385
Kris Chamberlin <kris.elliott.chamberlin@gmail.com>
2021-06-16 10:05:33 -0700

    Created config file for env variables (NOTE: these will be changed after deployment)

---
+++ next.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  env: {
+    mongodb_username: '06-dev-ag-law-14',
+    mongodb_password: 'ifrbrNi3iMRNjOM8',
+    mongodb_clustername: 'dev-ag-law',
+    mongodb_database: 'dev-ag-law',
+  },
+};
