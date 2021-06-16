c2b675883ac62e912ed63aa40724e99af3635041
Kris Chamberlin <kris.elliott.chamberlin@gmail.com>
2021-06-16 09:23:46 -0700

    Added own imports, removed CNA imports

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,11 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import { Fragment } from 'react';
+import FeaturedResults from '../components/home-page/featured-results';
+import Hero from '../components/home-page/hero';
+import AllPractices from '../components/practices/all-practices';
+import ContactForm from '../components/contact/contact-form';
+import { getPractices } from '../lib/practices-util';
+import { getFeaturedResults } from '../lib/results-util';
+import Head from 'next/head';
 
 export default function HomePage(props) {
   return (
